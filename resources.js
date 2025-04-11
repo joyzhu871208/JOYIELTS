d825ec04bda4303cf5f13c55e811f1153a502235
zhuqian <joyzhu871208@gmail.com>
2025-04-11 20:59:04 +0800

    Initial commit

---
+++ resources.js
@@ -0,0 +1,37 @@
+const resources = {
+  "ethanol-sample": {
+    title: "IELTS Task 1: Ethanol Process Diagram",
+    tasktype: "Cyclicle Process Diagram",
+    download: "resources/ethanol_sample.pdf",
+    preview: "",
+    video: ""
+  },
+  "line-graph": {
+    title: "IELTS Task 1: Line Graph Sample",
+    description: "折线图范文，适用于趋势变化类题型。",
+    download: "resources/line_graph_sample.pdf",
+    preview: "",
+    video: ""
+  },
+  "map-sample": {
+    title: "IELTS Task 1: Map Comparison Sample",
+    description: "地图类题型示例，包括A&B对比结构。",
+    download: "resources/map_sample.pdf",
+    preview: "",
+    video: ""
+  },
+  "pie-chart": {
+    title: "IELTS Task 1: Pie Chart Sample",
+    description: "饼图类范文，展示不同时间的数据分布。",
+    download: "resources/pie_chart_sample.pdf",
+    preview: "",
+    video: ""
+  },
+  "sugar-process": {
+    title: "IELTS Task 1: Sugar Process Diagram",
+    description: "展示糖类制作流程的写作范文。",
+    download: "resources/sugar_sample.pdf",
+    preview: "",
+    video: ""
+  }
+};
